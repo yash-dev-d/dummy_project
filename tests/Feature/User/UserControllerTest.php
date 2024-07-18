5e2bd29b6ea7a3a72267d1399b48ab961c7ac239
yash1th15 <yashwa491@gmail.com>
2024-07-18 14:55:55 +0530

    teardown_added

--- tests/Feature/User/UserControllerTest.php
+++ tests/Feature/User/UserControllerTest.php
@@ -23,7 +23,10 @@ class UserControllerTest extends TestCase
         $this->createApplication();
         $this->api_docs = new ApiDocs(get_class($this));
     }
-    
+    protected function tearDown(): void{
+        
+        parent::tearDown();
+      }
 
     use CreateUser;
     use GetUser;
